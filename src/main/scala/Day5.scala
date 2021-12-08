9a91edb0fa4926a9078267bd40e4ce1e69f0f41b
AlexandreG9 <alexandre.guiheneuf@iadvize.com>
2021-12-08 13:17:42 +0100

    day 5 completed

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -6,8 +6,10 @@ object Day5 extends App {
   val filename = "input-day5.txt"
   val input = Source.fromFile(filename).getLines.toList
 
+  // Sort
   val points = getPoints(input).map({
-    case (pA, pB) if pA.x > pB.x || pA.y > pB.y => (pB, pA)
+    case (pA, pB) if pA.x > pB.x => (pB, pA)
+    case (pA, pB) if pA.x == pB.x && pA.y > pB.y => (pB, pA)
     case tuple => tuple
   })
   val maxPoint = getMaxXY(points.flatMap(t => List(t._1, t._2)))
@@ -43,10 +45,22 @@ object Day5 extends App {
   // Calculate points from vector
   def getPointsFromVector(orig: Point, dest: Point): List[Point] = {
     // println(s"points from vector $orig $dest")
-    val useFunction = if (orig.x == dest.x) verticalCompute
-    else if (orig.y == dest.y) horizontalCompute
-    else if (orig.x < dest.x && orig.y < dest.y) diagonalDownCompute
-    else diagonalUpCompute
+    val useFunction = if (orig.x == dest.x) {
+      println(s"vertical $orig $dest")
+      verticalCompute
+    }
+    else if (orig.y == dest.y) {
+      println(s"horizontal $orig $dest")
+      horizontalCompute
+    }
+    else if (orig.x < dest.x && orig.y < dest.y) {
+      println(s"diag $orig $dest")
+      diagonalDownCompute
+    }
+    else {
+      println(s"diag $orig $dest")
+      diagonalUpCompute
+    }
 
     def loop(lastPoint: Point, acc: List[Point], func: (Int, Int) => Point): List[Point] = {
       // calculate point
