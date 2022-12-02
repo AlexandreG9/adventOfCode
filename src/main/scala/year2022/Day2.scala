7e1380bdf4248d34f348beb32761cdced7000b45
AlexandreG9 <alexandre.guiheneuf@iadvize.com>
2022-12-02 13:49:50 +0100

    day2 part 1

---
+++ src/main/scala/year2022/Day2.scala
@@ -0,0 +1,89 @@
+package year2022
+
+import cats.effect.{IO, IOApp}
+import year2022.utils.FileUtils.readLine
+
+trait Result {
+  def getScore: Int
+}
+case class Win() extends Result {
+  override def getScore: Int = 6
+}
+
+case class Draw() extends Result {
+  override def getScore: Int = 3
+}
+
+case class Lose() extends Result {
+  override def getScore: Int = 0
+}
+
+trait GameAction {
+
+  def getActionPoint: Int
+  def against(opponentAction: GameAction): Result
+}
+
+object GameAction {
+  def fromChar(value: Char): GameAction = {
+    if (value == 'A' || value == 'X') Rock()
+    else if (value == 'B' || value == 'Y') Paper()
+    else Scissors()
+  }
+}
+
+case class Scissors() extends GameAction {
+  override def against(opponentAction: GameAction): Result = opponentAction match {
+    case Paper() => Win()
+    case Rock() => Lose()
+    case Scissors() => Draw()
+    case _ => throw new IllegalArgumentException("unknown value")
+  }
+
+  override def getActionPoint: Int = 3
+}
+case class Rock() extends GameAction {
+  override def against(opponentAction: GameAction): Result = opponentAction match {
+    case Paper() => Lose()
+    case Rock() => Draw()
+    case Scissors() => Win()
+    case _ => throw new IllegalArgumentException("unknown value")
+  }
+
+  override def getActionPoint: Int = 1
+}
+case class Paper() extends GameAction {
+  override def against(opponentAction: GameAction): Result = opponentAction match {
+    case Paper() => Draw()
+    case Rock() => Win()
+    case Scissors() => Lose()
+    case _ => throw new IllegalArgumentException("unknown value")
+  }
+
+  override def getActionPoint: Int = 2
+}
+
+
+case class Round(opponentAction: GameAction, myAction: GameAction) {
+  def getScore: Int = myAction.against(opponentAction).getScore + myAction.getActionPoint
+}
+
+object Round {
+  def fromLines(input: Seq[String]): Seq[Round] = input.map(fromString)
+
+  private def fromString(line: String): Round = {
+    val opponent = GameAction.fromChar(line.charAt(0))
+    val myAction = GameAction.fromChar(line.charAt(2))
+    
+    Round(opponentAction = opponent, myAction = myAction)
+  }
+ }
+
+object Day2 extends IOApp.Simple {
+ val run = for {
+   lines <- readLine("/Users/alexandreguiheneuf/IdeaProjects/adventOfCode/src/resources/2022/input-day2.txt")
+   rounds = Round.fromLines(lines)
+   total = rounds.map(_.getScore).sum
+   _ <- IO.println(s"total score $total")
+ } yield ()
+}
