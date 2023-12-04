9d3bf64da1325e95cdff9656431f8d07ec4d20d9
AlexandreG9 <alexandre.guiheneuf@iadvize.com>
2023-12-04 14:27:30 +0100

    day3 part 2

--- src/main/scala/year2023/Day3.scala
+++ src/main/scala/year2023/Day3.scala
@@ -4,9 +4,13 @@ import cats.effect.{IO, IOApp}
 import year2022.utils.FileUtils.{readLine, readWholeFile}
 
 object Day3 extends IOApp.Simple {
-  val path = "/Users/alexandre/IdeaProjects/adventOfCode/2023/input-day3.txt"
+  val path = "/Users/alexandreguiheneuf/IdeaProjects/adventOfCode/2023/input-day3.txt"
 
   case class EngineNumber(start: Int, end: Int, line: Int, value: Int)
+
+  case class Position(x: Int, y: Int)
+
+  case class Gear(value: Int, position: Position)
 
   def findNumber(file: String): List[EngineNumber] = {
     val regex = """(\d+)""".r
@@ -27,7 +31,7 @@ object Day3 extends IOApp.Simple {
   def checkValidNumber(engineNumber: EngineNumber, file: String): Boolean = {
     val maxChar = file.split("\n")(engineNumber.line).length
 
-    println(s"engineNumber: ${engineNumber.line} ${engineNumber.start} ${engineNumber.end}")
+    // println(s"engineNumber: ${engineNumber.line} ${engineNumber.start} ${engineNumber.end}")
 
     val lineAbove = if (engineNumber.line > 0) getCharAtPosition(Math.max(engineNumber.start - 1, 0), Math.min(engineNumber.end + 1, maxChar), engineNumber.line - 1, file) else ""
     val lineBelow = if (engineNumber.line < file.split("\n").length - 1) getCharAtPosition(Math.max(engineNumber.start - 1, 0), Math.min(engineNumber.end + 1, maxChar), engineNumber.line + 1, file) else ""
@@ -46,14 +50,68 @@ object Day3 extends IOApp.Simple {
     lineToCheck.substring(start, end)
   }
 
+  def findGearPosition(file: String): List[Position] = {
+    val regex = """(\*)""".r
+    val lines = file.split("\n")
+
+    lines.zipWithIndex.flatMap { case (line, index) =>
+      regex.findAllMatchIn(line).map(m => {
+        Position(m.start, index)
+      }).toList
+    }.toList
+  }
+
+  def findGearValue(gearPosition: Position, engineNumber: List[EngineNumber], file: String): Option[Gear] = {
+    println(s"searching for gear: ${gearPosition}")
+
+    val maxChar = file.split("\n")(gearPosition.y).length
+    val maybeUpperEngineNumbers = if (gearPosition.y > 0) {
+      findEngineNumberWithPosition(Math.max(gearPosition.x - 1, 0), Math.min(gearPosition.x + 1, maxChar), gearPosition.y - 1, engineNumber)
+    } else Nil
+    val maybeDownEngineNumbers = if (gearPosition.y < maxChar) {
+      findEngineNumberWithPosition(Math.max(gearPosition.x - 1, 0), Math.min(gearPosition.x + 1, maxChar), gearPosition.y + 1, engineNumber)
+    } else Nil
+    val maybeLeft = if (gearPosition.x > 0) {
+      findEngineNumberWithPosition(gearPosition.x - 1, gearPosition.x - 1, gearPosition.y, engineNumber)
+    } else Nil
+    val maybeRight = if (gearPosition.x < maxChar) {
+      findEngineNumberWithPosition(gearPosition.x + 1, gearPosition.x + 1, gearPosition.y, engineNumber)
+    } else Nil
+
+
+    val maybeNumber = maybeUpperEngineNumbers ++ maybeDownEngineNumbers ++ maybeLeft ++ maybeRight
+
+    println("founded numbers: " + maybeNumber)
+
+    maybeNumber match {
+      case numbersList if numbersList.size == 2 => {
+        val value = numbersList.map(_.value).product
+        println(s"Numbers: ${numbersList}")
+        Some(Gear(value, gearPosition))
+      }
+      case _ => None
+    }
+  }
+
+  def findEngineNumberWithPosition(start: Int, end: Int, line: Int, engineNumber: List[EngineNumber]): List[EngineNumber] =
+    engineNumber.filter(engineNumber => engineNumber.line == line &&
+      ((engineNumber.start >= start && engineNumber.start <= end) ||
+        ((engineNumber.end - 1) >= start && (engineNumber.end - 1)  <= end)))
+
   override def run: IO[Unit] =
     for {
       file <- readWholeFile(path)
-      _ <- IO.println(s"${file}")
-      engineList = findNumber(file)
-      validEngineList = engineList.filter(engineNumber => checkValidNumber(engineNumber, file))
+      // _ <- IO.println(s"${file}")
+      engineNumberList = findNumber(file)
+      validEngineList = engineNumberList.filter(engineNumber => checkValidNumber(engineNumber, file))
       sumValue = validEngineList.map(_.value).sum
-      _ <- IO.println(s"${validEngineList.map(_.value)}")
-      _ <- IO.println(s"${sumValue}")
+      gears = findGearPosition(file)
+      validGears = gears.flatMap(gearPosition => findGearValue(gearPosition, engineNumberList, file))
+      _ <- IO.println(s"Engine number list: ${engineNumberList.map(_.value)}")
+      // _ <- IO.println(s"${validEngineList.map(_.value)}")
+      // _ <- IO.println(s"${sumValue}")
+      // _ <- IO.println(s"${gears}")
+      _ <- IO.println(s"Valid gears ${validGears}")
+      _ <- IO.println(s"Sum of valid gears ${validGears.map(_.value).sum}")
     } yield ()
 }
