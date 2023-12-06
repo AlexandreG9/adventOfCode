d1b430e98f5dd2d1643aa52a3422e16f985c3450
Alexandre Guiheneuf <aguiheneuf@gmail.com>
2023-12-06 20:31:12 +0100

    wip

--- src/main/scala/year2023/Day5.scala
+++ src/main/scala/year2023/Day5.scala
@@ -4,10 +4,11 @@ import year2022.utils.FileUtils.readWholeFile
 
 import scala.collection.immutable.NumericRange
 import scala.util.matching.Regex
-import cats.effect.{ IO, IOApp}
+import cats.effect.{IO, IOApp}
 import cats.implicits._
 
 import scala.concurrent.duration.Duration
+
 object Day5 extends IOApp.Simple {
   val path = "/Users/alexandre/IdeaProjects/adventOfCode/2023/input-day5.txt"
 
@@ -15,6 +16,10 @@ object Day5 extends IOApp.Simple {
     super.runtimeConfig.copy(cpuStarvationCheckInitialDelay = Duration.Inf)
 
   case class Seed(value: Long)
+
+  case class SeedRange(start: Long, end: Long) {
+    def contains(value: Long): Boolean = value >= start && value <= end
+  }
 
   case class Range(destinationStart: Long, sourceStart: Long, length: Long) {
     def isSupportedInput(value: Long): Boolean = value >= sourceStart && value <= sourceStart + length
@@ -44,6 +49,29 @@ object Day5 extends IOApp.Simple {
         case None => value
       }
     }
+
+    // not used
+    def upwardAll(value: Long, destination: String = "location"): Long = {
+      // println(s"upwardAll: $destination $value")
+      val maybeMap = maps.find(_.destination == destination)
+      maybeMap match {
+        case Some(map) => upwardAll(map.translate(value), map.origin)
+        case None => value
+      }
+    }
+ /**
+    def findIntervalForMinimumLocation(): InputRange = {
+      val locationMap = maps.find(_.destination == "location").get
+      val minDestinationRange = locationMap.ranges.sortBy(_.destinationStart).flatMap(possibleMinRange => {
+        val foundedSeeds = (possibleMinRange.sourceStart to possibleMinRange.sourceStart + possibleMinRange.length).map(upwardAll(_)).toList
+        foundedSeeds
+      }).toSet
+
+      println(s"foundedSeeds: ${minDestinationRange.size}")
+      println(s"foundedSeeds: ${minDestinationRange}")
+      ???
+    }
+*/
   }
 
   def readSeeds(file: String): List[Seed] = {
@@ -51,26 +79,18 @@ object Day5 extends IOApp.Simple {
     seeds.map(Seed)
   }
 
-  def readSeedsWithRange(file: String): IO[List[Seed]] = {
+
+  def readSeedsWithRange(file: String): List[SeedRange] = {
     val seedsPair = file.split("\n").head.replace("seeds: ", "")
     println(s"seedsPair: $seedsPair")
     val regexPair = """(\d+ \d+)""".r
 
-    def computeFromRegexMatches(m: Regex.Match): IO[List[Seed]] = IO {
+    regexPair.findAllMatchIn(seedsPair).map(m => {
       val values = m.group(1).split("\\s")
       val start = values(0).toLong
       val end = start + values(1).toLong
-
-      val estimateSize = (end - start) * 8 / 1024 / 1024
-      println(s"estimateSize: $estimateSize MB")
-      val seeds = NumericRange.inclusive(start, end, 1).toList.map(Seed)
-      println(s"seeds created for range: $start")
-      seeds
-    }
-
-    regexPair.findAllMatchIn(seedsPair).map(m => {
-      computeFromRegexMatches(m)
-    }).toList.sequence.map(_.flatten)
+      SeedRange(start, end)
+    }).toList
   }
 
   def readAlmanac(file: String): Almanac = {
@@ -94,19 +114,19 @@ object Day5 extends IOApp.Simple {
     Almanac(almanacMap)
   }
 
+  def countPossibleSeedsFromInput(inputRange: SeedRange): Long = {
+    inputRange.end - inputRange.start + 1
+  }
+
 
   override def run: IO[Unit] = {
     for {
       file <- readWholeFile(path)
       almanac = readAlmanac(file)
-      seeds <- readSeedsWithRange(file)
-      _ <- IO {
-        println(s"Seeds created, number of seeds ${seeds.size}")
-      }
-      seedsLocation = seeds.map(seed => almanac.forwardAll(seed.value))
-      _ <- IO {
-        println(seedsLocation.min)
-      }
+      seedRange = readSeedsWithRange(file)
+      possibleSeeds = seedRange.map(countPossibleSeedsFromInput).sum
+      _ = println(s"possibleSeeds: $possibleSeeds")
+
     } yield ()
   }
 }
