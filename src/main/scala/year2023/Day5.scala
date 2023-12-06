2eb748baeb46a993e527940d65449518e410b02a
AlexandreG9 <alexandre.guiheneuf@iadvize.com>
2023-12-06 17:47:08 +0100

    wip

--- src/main/scala/year2023/Day5.scala
+++ src/main/scala/year2023/Day5.scala
@@ -1,12 +1,18 @@
 package year2023
 
-import cats.effect.{IO, IOApp}
 import year2022.utils.FileUtils.readWholeFile
 
-import scala.collection.immutable
+import scala.collection.immutable.NumericRange
+import scala.util.matching.Regex
+import cats.effect.{ IO, IOApp}
+import cats.implicits._
 
+import scala.concurrent.duration.Duration
 object Day5 extends IOApp.Simple {
-  val path = "/Users/alexandreguiheneuf/IdeaProjects/adventOfCode/2023/input-day5.txt"
+  val path = "/Users/alexandre/IdeaProjects/adventOfCode/2023/input-day5.txt"
+
+  override def runtimeConfig =
+    super.runtimeConfig.copy(cpuStarvationCheckInitialDelay = Duration.Inf)
 
   case class Seed(value: Long)
 
@@ -30,11 +36,11 @@ object Day5 extends IOApp.Simple {
   }
 
   case class Almanac(maps: List[AlmanacMap]) {
-    def forwardAll(origin: String, value: Long): Long = {
+    def forwardAll(value: Long, origin: String = "seed"): Long = {
       println(s"forwardAll: $origin $value")
       val maybeMap = maps.find(_.origin == origin)
       maybeMap match {
-        case Some(map) => forwardAll(map.destination, map.translate(value))
+        case Some(map) => forwardAll(map.translate(value), map.destination)
         case None => value
       }
     }
@@ -45,16 +51,26 @@ object Day5 extends IOApp.Simple {
     seeds.map(Seed)
   }
 
-  def readSeedsWithRange(file: String): Set[Seed] = {
+  def readSeedsWithRange(file: String): IO[List[Seed]] = {
     val seedsPair = file.split("\n").head.replace("seeds: ", "")
+    println(s"seedsPair: $seedsPair")
     val regexPair = """(\d+ \d+)""".r
 
-    regexPair.findAllMatchIn(seedsPair).flatMap(m => {
-      val values = m.group(1).split(" ")
+    def computeFromRegexMatches(m: Regex.Match): IO[List[Seed]] = IO {
+      val values = m.group(1).split("\\s")
       val start = values(0).toLong
-      val end = values(1).toLong
-      start to end map Seed
-    }).toSet
+      val end = start + values(1).toLong
+
+      val estimateSize = (end - start) * 8 / 1024 / 1024
+      println(s"estimateSize: $estimateSize MB")
+      val seeds = NumericRange.inclusive(start, end, 1).toList.map(Seed)
+      println(s"seeds created for range: $start")
+      seeds
+    }
+
+    regexPair.findAllMatchIn(seedsPair).map(m => {
+      computeFromRegexMatches(m)
+    }).toList.sequence.map(_.flatten)
   }
 
   def readAlmanac(file: String): Almanac = {
@@ -83,11 +99,11 @@ object Day5 extends IOApp.Simple {
     for {
       file <- readWholeFile(path)
       almanac = readAlmanac(file)
-      seeds = readSeedsWithRange(file)
+      seeds <- readSeedsWithRange(file)
       _ <- IO {
-        println(s"number of seeds ${seeds.size}")
+        println(s"Seeds created, number of seeds ${seeds.size}")
       }
-      seedsLocation = seeds.map(seed => almanac.forwardAll("seed", seed.value))
+      seedsLocation = seeds.map(seed => almanac.forwardAll(seed.value))
       _ <- IO {
         println(seedsLocation.min)
       }
