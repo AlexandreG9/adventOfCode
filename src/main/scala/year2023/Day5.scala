9120c258924975431b5e59453fbd67fc1a6825ab
AlexandreG9 <alexandre.guiheneuf@iadvize.com>
2023-12-06 16:05:41 +0100

    solution part 1

--- src/main/scala/year2023/Day5.scala
+++ src/main/scala/year2023/Day5.scala
@@ -1,31 +1,72 @@
 package year2023
 
 import cats.effect.{IO, IOApp}
-import year2022.utils.FileUtils.{readLine, readWholeFile}
+import year2022.utils.FileUtils.readWholeFile
+
+import scala.collection.immutable
 
 object Day5 extends IOApp.Simple {
   val path = "/Users/alexandreguiheneuf/IdeaProjects/adventOfCode/2023/input-day5.txt"
 
-  case class Range(destinationStart: Int, sourceStart: Int, length: Int)
+  case class Seed(value: Long)
 
-  case class AlmanacMap(origin: String, destination: String, ranges: List[Range])
+  case class Range(destinationStart: Long, sourceStart: Long, length: Long) {
+    def isSupportedInput(value: Long): Boolean = value >= sourceStart && value <= sourceStart + length
 
-  case class Almanac(seed: List[Int], maps: List[AlmanacMap])
+    def translate(value: Long): Long = {
+      val delta = value - sourceStart
+      destinationStart + delta
+    }
+  }
+
+  case class AlmanacMap(origin: String, destination: String, ranges: List[Range]) {
+    def translate(value: Long): Long = {
+      val maybeRange = ranges.find(_.isSupportedInput(value))
+      maybeRange match {
+        case Some(range) => range.translate(value)
+        case None => value
+      }
+    }
+  }
+
+  case class Almanac(maps: List[AlmanacMap]) {
+    def forwardAll(origin: String, value: Long): Long = {
+      println(s"forwardAll: $origin $value")
+      val maybeMap = maps.find(_.origin == origin)
+      maybeMap match {
+        case Some(map) => forwardAll(map.destination, map.translate(value))
+        case None => value
+      }
+    }
+  }
+
+  def readSeeds(file: String): List[Seed] = {
+    val seeds = file.split("\n").head.replace("seeds: ", "").split(" ").map(_.toLong).toList
+    seeds.map(Seed)
+  }
+
+  def readSeedsWithRange(file: String): Set[Seed] = {
+    val seedsPair = file.split("\n").head.replace("seeds: ", "")
+    val regexPair = """(\d+ \d+)""".r
+
+    regexPair.findAllMatchIn(seedsPair).flatMap(m => {
+      val values = m.group(1).split(" ")
+      val start = values(0).toLong
+      val end = values(1).toLong
+      start to end map Seed
+    }).toSet
+  }
 
   def readAlmanac(file: String): Almanac = {
     val regexDeLaMort = """([a-z]+)-to-([a-z]+) map:\n((\d+(\s|\n))+)""".r
-
-    val seeds = file.split("\n").head.replace("seeds: ", "").split(" ").map(_.toInt).toList
-
     val almanacMap = regexDeLaMort.findAllMatchIn(file).map(m => {
       val origin = m.group(1)
       val destination = m.group(2)
       val ranges = m.group(3).split("\n").map(_.trim).filter(!_.isBlank).map(line => {
         val values = line.split(" ")
-
-        val destinationStart = values(0).toInt
-        val sourceStart = values(1).toInt
-        val length = values(2).toInt
+        val destinationStart = values(0).toLong
+        val sourceStart = values(1).toLong
+        val length = values(2).toLong
 
         Range(destinationStart, sourceStart, length)
       }).toList
@@ -34,17 +75,21 @@ object Day5 extends IOApp.Simple {
     }).toList
 
 
-    Almanac(seeds, almanacMap)
+    Almanac(almanacMap)
   }
 
 
   override def run: IO[Unit] = {
-
     for {
       file <- readWholeFile(path)
       almanac = readAlmanac(file)
+      seeds = readSeedsWithRange(file)
       _ <- IO {
-        println(almanac)
+        println(s"number of seeds ${seeds.size}")
+      }
+      seedsLocation = seeds.map(seed => almanac.forwardAll("seed", seed.value))
+      _ <- IO {
+        println(seedsLocation.min)
       }
     } yield ()
   }
