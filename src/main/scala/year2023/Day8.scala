7528d2a71b4ca4bf7d1204d4291d5b25fdcb2e2e
AlexandreG9 <alexandre.guiheneuf@iadvize.com>
2023-12-11 17:05:44 +0100

    day8 part2

--- src/main/scala/year2023/Day8.scala
+++ src/main/scala/year2023/Day8.scala
@@ -8,7 +8,11 @@ import scala.annotation.tailrec
 object Day8 extends IOApp.Simple {
   val path = "/Users/alexandreguiheneuf/IdeaProjects/adventOfCode/2023/input-day8.txt"
 
-  case class Node(name: String, left: String, right: String)
+  case class Node(name: String, left: String, right: String) {
+    private val _isEndNode = name.endsWith("Z")
+
+    def isEndNode = _isEndNode
+  }
 
   def traverseNodeStepsCount(nodeMap: Map[String, Node], instructionList: List[String]): Int = {
     @tailrec
@@ -22,6 +26,24 @@ object Day8 extends IOApp.Simple {
     }
 
     loop(0, nodeMap("AAA"), instructionList)
+  }
+
+  def traverseNodeStepCountV2(nodeMap: Map[String, Node], instructionList: List[String]): Long = {
+    def getNextNode(instruction: String, node: Node): Node =
+      nodeMap(if (instruction == "L") node.left else node.right)
+
+    def loop(acc: Long, nodes: List[Node], instructions: List[String]): Long = {
+      if (acc % 100000000 == 0) println(s"acc: $acc nodes: $nodes")
+      if (nodes.forall(_.isEndNode)) acc
+      else {
+        val nextInstructions = if (instructions.isEmpty) instructionList else instructions
+        val currentInstruction = nextInstructions.head
+
+        loop(acc + 1L, nodes.map(getNextNode(currentInstruction, _)), nextInstructions.tail)
+      }
+    }
+
+    loop(0L, nodeMap.filter(_._1.endsWith("A")).values.toList, instructionList)
   }
 
   def parseNodes(lines: List[String]): List[Node] = {
@@ -39,7 +61,9 @@ object Day8 extends IOApp.Simple {
     lines <- readLine(path)
     nodeMap = parseNodes(lines.toList).groupBy(_.name).map(elem => elem._1 -> elem._2.head)
     instructionsList = lines.head.toCharArray.toList.map(_.toString)
-    steps = traverseNodeStepsCount(nodeMap, instructionsList)
-    _ <- IO(println(steps))
+    // steps = traverseNodeStepsCount(nodeMap, instructionsList)
+    stepsv2 = traverseNodeStepCountV2(nodeMap, instructionsList)
+    // _ <- IO(println(steps))
+    _ <- IO(println(stepsv2))
   } yield ()
 }
